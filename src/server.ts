8311851a055b79d5daf1436d6f781781a79ff4d0
ricdnt <richard.donat@yahoo.fr>
2020-11-02 14:29:16 +0100

    clean to start react

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,23 @@
 import { Db } from "mongodb";
 import * as core from "express-serve-static-core";
 import express from "express";
+import mongoSession from "connect-mongo";
 import * as gamesController from "./controllers/games.controller";
 import * as nunjucks from "nunjucks";
 import * as platformsController from "./controllers/platforms.controller";
 import GameModel, { Game } from "./models/gameModel";
 import PlatformModel, { Platform } from "./models/platformModel";
 import bodyParser from "body-parser";
+import session from "express-session";
+import OAuth2Client, { OAuth2ClientConstructor } from "@fwl/oauth2";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const clientWantsJson = (request: express.Request): boolean => request.get("accept") === "application/json";
-
 const jsonParser = bodyParser.json();
 const formParser = bodyParser.urlencoded({ extended: true });
 
-export function makeApp(db: Db): core.Express {
+export function  makeApp(db: Db): core.Express {
   const app = express();
 
   nunjucks.configure("views", {
@@ -21,15 +25,61 @@ export function makeApp(db: Db): core.Express {
     express: app,
   });
 
+  const mongoStore = mongoSession(session);
+  if (process.env.NODE_ENV === "production") {
+    app.set("trust proxy", 1);
+  }
+
+  
+const sessionParser = session({
+    secret: process.env.CLIENT_SECRET || "",
+    name: "sessionId",
+    resave: false,
+    saveUninitialized: true,
+    store: new mongoStore({
+      client: Db,
+    }),
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      expires: new Date(Date.now() + 3600000),
+  },
+});
+
   app.use("/assets", express.static("public"));
   app.set("view engine", "njk");
 
   const platformModel = new PlatformModel(db.collection<Platform>("platforms"));
   const gameModel = new GameModel(db.collection<Game>("games"));
 
-  app.get("/", (_request, response) => response.render("pages/home"));
-  app.get("/api", (_request, response) => response.render("pages/api"));
+  // Authentification module
+  const oauthClientConstructorProps: OAuth2ClientConstructor = {
+    openIDConfigurationURL: "https://fewlines.connect.prod.fewlines.tech/.well-known/openid-configuration",
+    clientID: process.env.CLIENT_ID || "",
+    clientSecret: process.env.CLIENT_SECRET || "",
+    redirectURI: "http://localhost:8080/oauth/callback",
+    audience: process.env.AUDIENCE || "",
+    scopes: ["openid", "email"],
+  };
 
+
+  const oauthClient = new OAuth2Client(oauthClientConstructorProps);
+
+  console.log(oauthClient);
+
+  app.get("/", async (_request, response) => {
+    const param = (await oauthClient.getAuthorizationURL()).toString();
+    response.render("/layout/index", { param });
+  });
+
+  /*
+  app.get("/oauth/callback", sessionParser, (_request, response) => {
+
+  })
+*/
+  //// End of authentification module
+
+  app.get("/api", (_request, response) => response.render("pages/api"));
+  app.get("/", (_request, response) => response.render("pages/home"));
   app.get("/platforms", platformsController.index(platformModel));
   app.get("/platforms/new", platformsController.newPlatform());
   app.get("/platforms/:slug", platformsController.show(platformModel));
@@ -60,3 +110,4 @@ export function makeApp(db: Db): core.Express {
 
   return app;
 }
+
